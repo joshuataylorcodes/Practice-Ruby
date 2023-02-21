bb462129a3d13aa770b0ea8e649154c48d55b8c6
joshua <joshuamtaylor455@gmail.com>
2023-02-21 15:35:59 -0500

    java style

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+#ruby style
+
+shirts = { :color => "black", :style => "athletic", :material => "cotton" }
+shoes = { :color => "red", :style => "casual", :material => "leather" }
+pants = { :color => "blue", :style => "dress", :material => "polyester" }
+
+#java style {shirts = {color: "black", style:"athletic", material: "cotton"}shoes = {color: "red", style: "casual", material: "leather"}pants = {color: "blue", style: "dress", material: "polyester"}
+
+puts "#{pants[:color]} #{pants[:style]} #{pants[:material]} are the styles"
