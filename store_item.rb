3cee05c6b689b7475c37f47a9c0c6710e2825bfd
joshua <joshuamtaylor455@gmail.com>
2023-02-21 15:44:20 -0500

    change key names

--- store_item.rb
+++ store_item.rb
@@ -5,9 +5,9 @@ shoes = { :color => "red", :style => "casual", :material => "leather" }
 pants = { :color => "blue", :style => "dress", :material => "polyester" }
 
 #java style
-shirts1 = { color: "black", style: "athletic", material: "cotton" }
-shoes1 = { color: "red", style: "casual", material: "leather" }
-pants1 = { color: "blue", style: "dress", material: "polyester" }
+shirts2 = { color: "black", style: "athletic", material: "cotton" }
+shoes2 = { color: "red", style: "casual", material: "leather" }
+pants2 = { color: "blue", style: "dress", material: "polyester" }
 
 puts "#{pants[:color]} #{pants[:style]} #{pants[:material]} are the styles"
 puts "#{pants1[:color]} #{pants1[:style]} #{pants1[:material]} are the styles"
