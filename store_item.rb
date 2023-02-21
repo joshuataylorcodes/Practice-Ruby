e3edc2947baf570a1e4277f41712b984e7b16932
joshua <joshuamtaylor455@gmail.com>
2023-02-21 15:39:52 -0500

    ruby and java style

--- store_item.rb
+++ store_item.rb
@@ -4,6 +4,10 @@ shirts = { :color => "black", :style => "athletic", :material => "cotton" }
 shoes = { :color => "red", :style => "casual", :material => "leather" }
 pants = { :color => "blue", :style => "dress", :material => "polyester" }
 
-#java style {shirts = {color: "black", style:"athletic", material: "cotton"}shoes = {color: "red", style: "casual", material: "leather"}pants = {color: "blue", style: "dress", material: "polyester"}
+#java style
+shirts1 = { color: "black", style: "athletic", material: "cotton" }
+shoes1 = { color: "red", style: "casual", material: "leather" }
+pants1 = { color: "blue", style: "dress", material: "polyester" }
 
 puts "#{pants[:color]} #{pants[:style]} #{pants[:material]} are the styles"
+puts "#{pants1[:color]} #{pants1[:style]} #{pants1[:material]} are the styles"
