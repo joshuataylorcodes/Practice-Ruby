02ea01fee1207289c6d8c0ec1d19cff4739bec48
joshua <joshuamtaylor455@gmail.com>
2023-02-23 12:37:59 -0500

    Module Example using Modules instead of inheritance

---
+++ modlue_example.rb
@@ -0,0 +1,45 @@
+module VehicleAttributes
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include VehicleAttributes
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include VehicleAttributes
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+bike = Bike.new
+
+bike.accelerate
+car.accelerate
+p car
+p bike
+
+car.honk_horn
+bike.ring_bell
